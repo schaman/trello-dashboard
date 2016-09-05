c52b92d422daca085a41bd90065647f9cb5fef8b
Nick Vorobiov <schaman@yandex.ru>
2016-09-05 14:57:52 +0300

    tooltip

--- js/flow.js
+++ js/flow.js
@@ -1,39 +1,9 @@
 var data = [];
-
-var barHeight = 20;
-
-var margin = {top: 5, right: 5, bottom: 20, left: 100},
-    width = 960 - margin.left - margin.right;
-
-var height = barHeight * data.length; // update me
-
-// x – time scale
-var x = d3.scaleTime()
-    .domain([new Date("2016-06-14T10:49:02.000Z"), new Date("2016-09-05T08:30:09.839Z")])
-//    .domain([
-//      d3.min(data, function(d){ return d.start }),
-//      d3.max(data, function(d){ return d.finish })])
-    .range([0, width]);
-
-// y - person scale
-var y = d3.scaleBand();
+var totalStudentCount;
+var states = {};
 
 // z - state scale
 var z = d3.scaleOrdinal();
-
-// set size and margins
-var chart = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-//    .attr("height", barHeight * data.length + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var xAxis = d3.axisBottom(x);
-
-chart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + barHeight * data.length + ")")
-    .call(xAxis);
 
 // подбираем цвет для каждого состояния
 function setStates(lists) {
@@ -46,34 +16,73 @@ function setStates(lists) {
   z.range(lists.map(function(item, i, a){
     return color(i / a.length);
   }))
+
+  lists.map(function(list){
+    states[list.id] = list;
+  })
 }
 
-function render(card, story) {
-  data.push({
-    card: card,
-    story: story
-  })
+function render() {
+  var barHeight = 20;
 
-  height = barHeight * data.length; // update me
+  var margin = {top: 5, right: 5, bottom: 20, left: 130},
+      width = 960 - margin.left - margin.right;
 
-  y.domain(data.map(function(d) { /*console.log(d.card.name);*/ return d.card.name; }));
+  var height = barHeight * data.length;
+
+  // set size and margins
+  var chart = d3.select(".chart")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", barHeight * data.length + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // x – time scale
+
+  var x = d3.scaleTime()
+      .domain([
+        d3.min(data, function(person){
+          return d3.min(person.story, function(d){ return d.start })
+        }),
+        d3.max(data, function(person){
+          return d3.max(person.story, function(d){ return d.finish })
+        })
+      ])
+      .range([0, width]);
+
+  var xAxis = d3.axisBottom(x);
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + barHeight * data.length + ")")
+      .call(xAxis);
+
+  // y - person scale
+  var y = d3.scaleBand();
+
+  y.domain(data.map(function(d) { return d.card.name; }));
   y.rangeRound([0, height]);
 
   var yAxis = d3.axisLeft(y);
+
   chart.append("g")
       .attr("class", "y axis")
       .call(yAxis);
 
-  // add height to chart
-  chart.attr("height", height + margin.top + margin.bottom);
-  chart.select(".x").attr("transform", "translate(0," + barHeight * data.length + ")");
+  // tip
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      var start = moment(d.start);
+      var finish = moment(d.finish);
+      //return states[d.state].name + ' ' + moment(d.start).format('D MMM') + ' – ' + moment(d.finish).format('D MMM');
+      return states[d.state].name + ' ' + 
+        start.format('D MMM') + ' – ' + finish.format('D MMM') +
+        ' (' + finish.diff(start, 'days') + ' days)';
+    })
 
-  // check bounds
-  story.map(function(item){
-    if (x.domain()[0] > item.start) {
-      x.domain([item.start, x.domain()[1]])
-    }
-  })
+  chart.call(tip);
 
   // shift every next bar down
   var person = chart.selectAll("g.person")
@@ -92,15 +101,10 @@ function render(card, story) {
       .attr("x", function(d) { return x(d.start); })
       .attr("fill", function(d) { return z(d.state); })
       .attr("width", function(d) { return x(d.finish) - x(d.start); })
-      .attr("height", barHeight - 1);
+      .attr("height", barHeight - 1)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
 
-/*
-  bar.append("text")
-      .attr("x", function(d) { return x(d.finish) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.state; });
-*/
 }
 
 function tellStory(card, story){
@@ -116,17 +120,14 @@ function tellStory(card, story){
     });
   }
 
-  render(card, scenes);
-
-/*
-  $(function(){
-    $('#content').append(
-      '<h5>' + card.name + '</h5>' +
-      '<pre>' + JSON.stringify(scenes, null, 2) + '</pre>' +
-      '<pre>' + JSON.stringify(story, null, 2) + '</pre>'
-    );
+  data.push({
+    card: card,
+    story: scenes
   })
-*/
+
+  if (data.length == totalStudentCount) {
+    render();
+  }
 }
 
 document.addEventListener('trelloReady', function(event){
@@ -137,6 +138,9 @@ document.addEventListener('trelloReady', function(event){
       // убираем карточки в списке МЕТА и карточки с лейблами
       return (excludeListIds.indexOf(card.idList) < 0) && (card.idLabels.length == 0);
     })
+
+    // запоминаем, сколько всего студентов
+    totalStudentCount = cards.length;    
 
     // 575fe11ee7cfb0b5b5bc7814
     //cards = cards.filter(function(card){
